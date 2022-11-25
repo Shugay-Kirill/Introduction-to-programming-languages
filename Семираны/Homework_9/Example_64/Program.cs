e3cfa3d304aac19a38d2cd834cd24902f178bef8
Kilill_Shugay <shugaykirilll@gmail.com>
2022-11-25 10:37:31 +0200

    homework_9

---
+++ Семираны/Homework_9/Example_64/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string PrintNumber (int number = 8)
+{
+    string result = string.Empty;
+    if(number == 0) return result;
+    if(number == 1) return result + number + PrintNumber(number-1);
+    return result + number +", " + PrintNumber(number-1);
+}
+
+Console.WriteLine(PrintNumber());
---
+++ Семираны/Homework_9/Example_66/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SummNumber(int M = 4, int N = 8)
+{
+    int summ =+ M;
+    if (M == N) return summ;
+    return summ  + SummNumber(M + 1, N);
+}
+Console.WriteLine($"summ = {SummNumber()}");
